4a3d4b7989e9bb62eb074bac86daa6891f1b148f
Spencer Krum <nibz@spencerkrum.com>
2020-05-18 23:29:53 -0500

    Multiple fixes/improvements (#1)
    * More flexible with content-type
    
    js/axios has a default content type of "application/json;charset=utf8"
    so the previous strict checking was too restrictive
    
    * Parse map and player data for state api
    
    * Parse each player line in status output
    
    Structured player data, yay!

--- api/v1/state.go
+++ api/v1/state.go
@@ -2,6 +2,26 @@ package v1
 
 // StatusResponse is a summary status for the server
 type StatusResponse struct {
-	Hostname string `json:"hostname"`
-	Version  string `json:"version"`
+	Hostname string        `json:"hostname"`
+	Version  string        `json:"version"`
+	Map      string        `json:"map"`
+	Players  PlayersObject `json:"players"`
+}
+
+type PlayersObject struct {
+	HumanPlayers int      `json:"human_players"`
+	BotPlayers   int      `json:"bot_players"`
+	Players      []Player `json:"players"`
+}
+
+type Player struct {
+	Userid        string `json:"user_id"`
+	Name          string `json:"name"`
+	Uniqueid      string `json:"unique_id"`
+	TimeConnected string `json:"time_connected"`
+	Ping          int    `json:"ping"`
+	Loss          int    `json:"loss"`
+	State         string `json:"state"`
+	Rate          string `json:"rate"`
+	Address       string `json:"address"`
 }
--- stateserver.go
+++ stateserver.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/gorilla/mux"
@@ -50,15 +51,79 @@ func (s *StateServer) handleStatus(w http.ResponseWriter, req *http.Request) {
 }
 
 func statusToResponse(status string) *v1.StatusResponse {
-	statusSplit := strings.SplitN(status, "\n", 3)
+	statusSplit := strings.SplitN(status, "\n", 8)
 
 	hostname := removeFieldPrefix(statusSplit[0])
 	version := removeFieldPrefix(statusSplit[1])
+	map_name := removeFieldPrefix(statusSplit[5])
+	playersline := removeFieldPrefix(statusSplit[6])
+	rest := statusSplit[7]
+
+	humans, bots := parsePlayersLine(playersline)
+	allPlayers := parseAllPlayers(rest)
+	players := v1.PlayersObject{HumanPlayers: humans, BotPlayers: bots, Players: allPlayers}
 
 	return &v1.StatusResponse{
 		Hostname: hostname,
 		Version:  version,
+		Map:      map_name,
+		Players:  players,
 	}
+}
+
+func parsePlayersLine(line string) (int, int) {
+	lineSplit := strings.SplitN(line, " ", 4)
+	humans, err := strconv.Atoi(lineSplit[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	bots, err := strconv.Atoi(lineSplit[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return humans, bots
+
+}
+
+func parseAllPlayers(rest string) []v1.Player {
+	players := make([]v1.Player, 0)
+	//clean up input string
+	trimmed := strings.TrimPrefix(strings.TrimSuffix(rest, "\n#end\n"), "\n")
+	lines := strings.Split(trimmed, "\n")
+	for idx, line := range lines {
+		// removes header line
+		if idx == 0 {
+			continue
+		}
+		split := strings.Split(line, " ")
+		// skip bots
+		if split[2] == "BOT" {
+			continue
+		}
+		ping, err := strconv.Atoi(split[6])
+		if err != nil {
+			log.Fatal(err)
+		}
+		loss, err := strconv.Atoi(split[7])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		player := v1.Player{
+			Userid:        split[1],
+			Name:          split[3],
+			Uniqueid:      split[4],
+			TimeConnected: split[5],
+			Ping:          ping,
+			Loss:          loss,
+			State:         split[8],
+			Rate:          split[9],
+			Address:       split[10],
+		}
+		players = append(players, player)
+
+	}
+	return players
 }
 
 func removeFieldPrefix(line string) string {
